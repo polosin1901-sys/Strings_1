9d38071ab4b15ec3a93e716be721c599af7ba866
n.polosin <polosin1901@gmail.com>
2026-06-24 10:04:04 +0300

    Done hw2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,53 @@
+import java.time.LocalDate;
+
 public class Main {
+    // 1 задача
+    public static void defineYear(int definedYear) {
+        if (definedYear < 1584) {
+            System.out.println("В " + definedYear + " году ещё не было введено разделение на високосные и не високосные года");
+        } else if ((definedYear % 4 == 0 && definedYear % 100 != 0) || (definedYear % 400 == 0)) {
+            System.out.println(definedYear + " год является високосным");
+        } else {
+            System.out.println(definedYear + " год не является високосным");
+        }
+    }
+
+    // 2 задача
+    public static void displayMessage(int clientDeviceYear, int clientOS) {
+        if (clientDeviceYear < 2015 && clientOS == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientDeviceYear >= 2015 && clientOS == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientDeviceYear >= 2015 && clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+
+    //    3 задача
+    public static void defineDaysOfDelivery(int deliveryDistance) {
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется 1 день");
+        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+            System.out.println("Потребуется 2 дня");
+        } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+            System.out.println("Потребуется 3 дня");
+        } else {
+            System.out.println("Доставки нет");
+        }
+    }
+
     public static void main(String[] args) {
-// 1 задача
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
-        String fullName = lastName + " " + firstName + " " + middleName;
-        System.out.println("Ф.И.О. сотрудника — " + fullName);
-// 2 задача
-        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
-//  3 задача
-        fullName = "Иванов Семён Семёнович";
-        System.out.println("Данные Ф.И.О. сотрудника — " + fullName.replace('ё', 'е'));
+//        Проверка для метода из 1 задачи
+        int year = 2021;
+        defineYear(year);
+// Проверка для метода из 2 задачи
+        int clientOS1 = 0;
+        int currentYear1 = 2015;
+        displayMessage(currentYear1, clientOS1);
+// Проверка для метода из 3 задачи
+        int deliveryDistance1 = 95;
+        defineDaysOfDelivery(deliveryDistance1);
     }
 }
